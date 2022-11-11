de4ca81ba876766059fa1aa9fe6a605d7dc45061
REALSTEVEIG <101066723+REALSTEVEIG@users.noreply.github.com>
2022-11-11 23:58:53 +0100

    third commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('express-async-errors')
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const connectDB = require('./db/connect')
 const router = require('./routes/auth')
 const {engine} = require('express-handlebars')
